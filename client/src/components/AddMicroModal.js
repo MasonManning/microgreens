25b2b939325a83b076e3862bbb40dfd96151d19f
MasonManning <MasonManning@protonmail.com>
2020-09-09 23:23:01 +1000

    Update MicroModal UI and post new microgreen to server

--- client/src/components/AddMicroModal.js
+++ client/src/components/AddMicroModal.js
@@ -1,64 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
 import Table from 'react-bootstrap/Table'
+import Form from 'react-bootstrap/Form'
 import './AddMicroModal.css'
 AddMicroModal.propTypes = {
 
 };
 
 function AddMicroModal(props) {
+    const [seed, setSeed] = useState('')
+    const [seedQty, setSeedQty] = useState('')
+    const [soilType, setSoilType] = useState('')
+    const [stage, setStage] = useState('')
+    const [notes, setNotes] = useState('')
     console.log(props.show)
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log("Submit")
+        console.log(event.target)
+        console.log(seed)
+
+        fetch('/api/microgreens', {
+            method: 'POST',
+            headers: {
+                'Accept': 'Application/json',
+                'Content-Type': 'Application/json'
+            },
+            body: JSON.stringify({
+                seed: seed,
+                seedQty: seedQty,
+                soilType: soilType,
+                stage: stage,
+                notes: notes
+            })
+        })
+    }
     return (
         <div>
             {/* <Modal show={props.show} onHide={props.handleClose} dialogClassName="modal-dialog modal-xl" > */}
-            <Modal show={props.show} onHide={props.handleClose} dialogClassName="modal-90w">
+            <Modal show={props.show} onHide={props.handleClose} >
                 <Modal.Header closeButton></Modal.Header>
                 <Modal.Body>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Control placeholder='Seed' onChange={(e) => { setSeed(e.target.value) }} />
+                        <Form.Control placeholder='Seed Qty' onChange={(e) => { setSeedQty(e.target.value) }} />
+                        <Form.Control placeholder='Soil Type' onChange={(e) => { setSoilType(e.target.value) }} />
+                        <Form.Control placeholder='Stage' onChange={(e) => { setStage(e.target.value) }} />
+                        <Form.Control placeholder='Notes' onChange={(e) => { setNotes(e.target.value) }} />
+                    </Form>
                     <Table striped bordered hover>
-                        <thead>
-                            <tr>
-                                <th>
-                                    Seed
-                                </th>
-                                <th>
-                                    Seed Qty
-                                </th>
-                                <th>
-                                    Soil Type
-                                </th>
-                                <th>
-                                    Stage
-                                </th>
-                                <th>
-                                    Notes
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <input type='text' placeholder="Enter A Seed Type"/>
-                                </td>
-                                <td>
-                                    <input type='text' placeholder="Enter A Seed Qty"/>
-                                </td>
-                                <td>
-                                    <input type='text' placeholder="Enter A Soil Type"/>
-                                </td>
-                                <td>
-                                    <input type='text' placeholder="Leave Blank To Default To Germination"/>
-                                </td>
-                                <td>
-                                    <input type='text' placeholder="Enter Notes"/>
-                                </td>
-                            </tr>
-                        </tbody>
                     </Table>
-                    Table With Inputs Here
                 </Modal.Body>
                 <Modal.Footer>
+                    <Button type='submit'>Submit</Button>
                     <Button onClick={props.handleClose}>Close</Button>
                 </Modal.Footer>
             </Modal>
--- server/controller/apiController.js
+++ server/controller/apiController.js
@@ -7,12 +7,17 @@ module.exports = function (app) {
     
     app.get('/api/microgreens', (req, res)=>{
         Microgreen.find({}, (err, microgreen)=>{
+            console.log(err)
             if(err)throw err
             res.send(microgreen)
         })
     })
 
     app.post('/api/microgreens', (req, res) => {
+        console.log("Post   Microgreens")
+        // console.log(req.body.seed)
+        console.log(req.body)
+
         if (req.body.id) {
             Microgreen.findByIdAndUpdate(req.body.id, {
                 seed: req.body.seed,
