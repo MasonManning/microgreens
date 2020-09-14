905603cbbff40fdb776d0c97eee5e80cc67ed909
MasonManning <MasonManning@protonmail.com>
2020-09-14 23:39:13 +1000

    Add redux template

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux'
+import microgreenReducer from './reducers/microgreens'
+
+const store = createStore(microgreenReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ client/src/reducers/microgreens.js
@@ -0,0 +1,11 @@
+
+const microgreens = (state = 0, action) => {
+    switch (action.type) {
+        case 'ADD_MICROGREEN':
+            return state
+        default:
+            return state
+    }
+}
+
+export default microgreens
